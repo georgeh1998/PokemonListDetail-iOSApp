619039de890d207c0396af4ea52c1372ed3360a5
georgeh1998 <gouta.gorogoro@gmail.com>
2024-12-31 14:01:43 +0900

    Initial Commit

---
+++ PokemonListDetail-iOSApp/PokemonListDetail_iOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonListDetail_iOSAppApp.swift
+//  PokemonListDetail-iOSApp
+//
+//  Created by Hasegawa Gota on 2024/12/31.
+//
+
+import SwiftUI
+
+@main
+struct PokemonListDetail_iOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
